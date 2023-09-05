336e4c103108bb3ef3714b43c95176eea5dcdcff
Garrett Epperson <garrett.epperson@priority1.com>
2023-09-05 09:43:55 -0500

    add header

--- IronPdf-Header-Footer-Href-Bug/Program.cs
+++ IronPdf-Header-Footer-Href-Bug/Program.cs
@@ -2,17 +2,21 @@
 IronPdf.Logging.Logger.LoggingMode = IronPdf.Logging.Logger.LoggingModes.File;
 IronPdf.Logging.Logger.LogFilePath = "./IronPdfLog.txt";
 
-var bodyHtml = """<a href="https://google.com" id="test" target="_blank">This link works</a>""";
-var footerHtml = """<a href="https://google.com" id="test" target="_blank">This link doesn't</a>""";
+var bodyHtml = """<a href="https://google.com" target="_blank">This link works</a>""";
+var headerFooterHtml = """<a href="https://google.com" target="_blank">This link doesn't work</a>""";
 
 var pdf = ChromePdfRenderer.StaticRenderHtmlAsPdf(
     bodyHtml, 
     new ChromePdfRenderOptions
     {
+        HtmlHeader = new HtmlHeaderFooter
+        {
+            HtmlFragment = headerFooterHtml
+        },
         HtmlFooter = new HtmlHeaderFooter
         {
-            HtmlFragment = footerHtml
-        },
+            HtmlFragment = headerFooterHtml
+        }
     });
 
 File.WriteAllBytes("./Output.pdf", pdf.BinaryData);
