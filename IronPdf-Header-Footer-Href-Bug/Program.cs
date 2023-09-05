81069392ca888294debc3977dd0cacd9ccb38342
Garrett Epperson <garrett.epperson@priority1.com>
2023-09-05 09:40:59 -0500

    Add project files.

---
+++ IronPdf-Header-Footer-Href-Bug/Program.cs
@@ -0,0 +1,18 @@
+﻿IronPdf.License.LicenseKey = "";
+IronPdf.Logging.Logger.LoggingMode = IronPdf.Logging.Logger.LoggingModes.File;
+IronPdf.Logging.Logger.LogFilePath = "./IronPdfLog.txt";
+
+var bodyHtml = """<a href="https://google.com" id="test" target="_blank">This link works</a>""";
+var footerHtml = """<a href="https://google.com" id="test" target="_blank">This link doesn't</a>""";
+
+var pdf = ChromePdfRenderer.StaticRenderHtmlAsPdf(
+    bodyHtml, 
+    new ChromePdfRenderOptions
+    {
+        HtmlFooter = new HtmlHeaderFooter
+        {
+            HtmlFragment = footerHtml
+        },
+    });
+
+File.WriteAllBytes("./Output.pdf", pdf.BinaryData);
